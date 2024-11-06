232375dd83b3c7526039f96c8122819f66292908
zhar <91694359+zh4r@users.noreply.github.com.>
2024-11-06 23:49:34 +0200

    Mail validation struggle

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-const mailInput = document.getElementById('email');
-const email = input.value;
+// The browser seems to be validating the mail by itself - have to check how to access this, and use it for a simpler function.
 
-function validateEmail(email) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
+
+
+const mailInput = document.getElementById('email').value;
+const mailErr = document.getElementById('email-error');
+
+function validateEmail(mailInput) {
+    // const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    // console.log(mailInput)
+    if (!mailInput.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+        mailErr.innerHTML = "Enter Valid mail";
+        return false;
+    };
+
+    mailErr.HTML = "";
+    return true;
+    // return emailRegex.test(mailInput);
 }
 
-addEventListener("input", (event) => {});
+// mailInput.addEventListener
 
-oninput = (event) => {};
+// oninput = (event) => {};
 
+const works = () => console.log('working');
+
+works();
 
 //  https://developer.mozilla.org/en-US/docs/Web/API/Element/input_event
+
+
