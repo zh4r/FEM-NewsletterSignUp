3a36fc5ececced756a37b483dcff76ce9134df53
zhar <91694359+zh4r@users.noreply.github.com.>
2024-11-10 20:23:22 +0200

    switch to using only one html. TODO: Fix scripts + CSSMess Cleanup

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
+const thanks = document.getElementById('thanks');
+const main = document.querySelector('main');
+const button = document.querySelector('button');
+
+thanks.classList.add('hide');
+
 function validateEmail() {
     const mailErr = document.getElementById('email-error');
     const emailInput = document.getElementById('email');
     const email = emailInput.value;
+    let mailSpan = document.getElementById('mailSpan').value;
     const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    const button = document.querySelector('button');
 
     if (!emailPattern.test(email)) {
         emailInput.classList.add('invalidMail');
         mailErr.style.display = 'block';
         button.disabled = true;
+
     } else {
         emailInput.classList.remove('invalidMail');
         mailErr.style.display = 'none';
         button.disabled = false;
+        mailSpan = email;
     }
 }
+
+function hideMain() {
+    main.classList.add('hide');
+    thanks.classList.remove('hide');
+} 
+
+function hideThanks() {
+    thanks.classList.add('hide');
+    main.classList.remove('hide');
+} 
