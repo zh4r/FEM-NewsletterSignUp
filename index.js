880bd736d6649ef87e3befd928ee69104ed7695f
zh4r <zh4r@proton.me>
2024-11-11 21:58:05 +0200

    Script working; TODO: reformat some parts of JS and clean up CSS after

--- index.js
+++ index.js
@@ -1,35 +1,45 @@
 const thanks = document.getElementById('thanks');
 const main = document.querySelector('main');
 const button = document.querySelector('button');
-
+let mailSpan = document.getElementById('mailSpan');
 thanks.classList.add('hide');
 
 function validateEmail() {
-    const mailErr = document.getElementById('email-error');
     const emailInput = document.getElementById('email');
     const email = emailInput.value;
-    let mailSpan = document.getElementById('mailSpan').value;
+    const mailErr = document.getElementById('email-error');
     const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
     if (!emailPattern.test(email)) {
         emailInput.classList.add('invalidMail');
         mailErr.style.display = 'block';
         button.disabled = true;
-
+        console.log(email);
     } else {
         emailInput.classList.remove('invalidMail');
         mailErr.style.display = 'none';
         button.disabled = false;
-        mailSpan = email;
+        console.log(email);
     }
 }
 
 function hideMain() {
-    main.classList.add('hide');
-    thanks.classList.remove('hide');
-} 
+    const email = document.getElementById('email').value;
+    thanks.classList.toggle('hide');
+    main.classList.toggle('hide');
+    mailSpan.textContent = email;
+}
 
-function hideThanks() {
-    thanks.classList.add('hide');
-    main.classList.remove('hide');
-} 
+// function hideThanks() {
+//     thanks.classList.add('hide');
+//     main.classList.remove('hide');
+//     mailSpan.textContent = '';
+// }
+
+button.addEventListener('click', (event) => {
+    event.preventDefault();
+    validateEmail();
+    if (!button.disabled) {
+    hideMain();
+    }
+})
