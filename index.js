6d9a840cbd412cbf6b181b0d3c120321c80a14d0
zh4r <zh4r@proton.me>
2024-11-12 23:56:27 +0200

    Reformat small things like function names and hover effects; TODO: Make Dismiss btn work with Enter + CSSMess

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const thanks = document.getElementById('thanks');
 const main = document.querySelector('main');
 const button = document.querySelector('button');
+const emailInput = document.getElementById('email');
 let mailSpan = document.getElementById('mailSpan');
 thanks.classList.add('hide');
 
 function validateEmail() {
-    const emailInput = document.getElementById('email');
     const email = emailInput.value;
     const mailErr = document.getElementById('email-error');
     const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
@@ -14,32 +14,33 @@ function validateEmail() {
         emailInput.classList.add('invalidMail');
         mailErr.style.display = 'block';
         button.disabled = true;
-        console.log(email);
     } else {
         emailInput.classList.remove('invalidMail');
         mailErr.style.display = 'none';
         button.disabled = false;
-        console.log(email);
     }
-}
+};
 
-function hideMain() {
+function toggleHide() {
     const email = document.getElementById('email').value;
     thanks.classList.toggle('hide');
     main.classList.toggle('hide');
     mailSpan.textContent = email;
-}
-
-// function hideThanks() {
-//     thanks.classList.add('hide');
-//     main.classList.remove('hide');
-//     mailSpan.textContent = '';
-// }
+};
 
 button.addEventListener('click', (event) => {
     event.preventDefault();
     validateEmail();
     if (!button.disabled) {
-    hideMain();
+        toggleHide();
     }
-})
+});
+
+    //TODO: Dismiss button doesn't work with enter.
+button.addEventListener('keypress', (event) => {
+    event.preventDefault();
+    if (event.key === "Enter") {
+        event.preventDefault();
+        document.getElementById("dismiss").click();
+    }
+});
